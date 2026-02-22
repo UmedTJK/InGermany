0ed1eee43aa7e71679718105259dd1a30232a912
UmedTJK <mailmeefree@gmail.com>
2026-02-22 20:07:27 +0100

    ui(debt): remove unused applyCardStyle legacy extension

--- UIUtils/CardStyle.swift
+++ UIUtils/CardStyle.swift
@@ -30,36 +30,3 @@ enum CardStyle: String, CaseIterable, Identifiable, Codable {
         }
     }
 }
-
-extension View {
-    /// Applies a card style dynamically based on the provided `CardStyle`.
-    func applyCardStyle(_ style: CardStyle) -> some View {
-        switch style {
-        case .standard:
-            return AnyView(
-                self
-                    .background(Color(.systemBackground))
-                    .cornerRadius(12)
-                    .shadow(
-                        color: Theme.cardShadow.color,
-                        radius: Theme.cardShadow.radius,
-                        x: Theme.cardShadow.x,
-                        y: Theme.cardShadow.y
-                    )
-            )
-        case .light:
-            return AnyView(
-                self
-                    .background(Color(.systemBackground))
-                    .cornerRadius(10)
-                    .shadow(
-                        color: Theme.lightShadow.color,
-                        radius: Theme.lightShadow.radius,
-                        x: Theme.lightShadow.x,
-                        y: Theme.lightShadow.y
-                    )
-            )
-        }
-    }
-}
-
