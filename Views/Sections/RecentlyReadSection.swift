a48810475e6fa49860f958aa34d69f53ff430634
UmedTJK <mailmeefree@gmail.com>
2026-02-23 19:30:28 +0100

    RecentlyRead: apply card press feedback to article cards

--- Views/Sections/RecentlyReadSection.swift
+++ Views/Sections/RecentlyReadSection.swift
@@ -51,6 +51,7 @@ struct RecentlyReadSection: View {
                             )
                         }
                         .buttonStyle(.plain)
+                        .cardPressFeedback()
                     }
                 }
             }
