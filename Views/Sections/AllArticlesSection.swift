e2d8c8f685257b0422770d0efb3bc3e295acfa4a
UmedTJK <mailmeefree@gmail.com>
2026-02-23 20:33:26 +0100

    AllArticles: apply card press feedback

--- Views/Sections/AllArticlesSection.swift
+++ Views/Sections/AllArticlesSection.swift
@@ -46,6 +46,7 @@ struct AllArticlesSection: View {
                         )
                     }
                     .buttonStyle(.plain)
+                    .cardPressFeedback()
                 }
             }
         }
